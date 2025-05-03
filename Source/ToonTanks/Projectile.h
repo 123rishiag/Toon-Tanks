ff420eda462c26991b2ba03644349beb97a6c28d
Rishabh Garg <narishabh.garg@gmail.com>
2025-05-03 20:02:15 +0530

    Added Launch, Hit and Death Sounds

--- Source/ToonTanks/Projectile.h
+++ Source/ToonTanks/Projectile.h
@@ -39,6 +39,12 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Combat")
 	class UParticleSystemComponent* TrailParticles;
 
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	class USoundBase* LaunchSound;
+
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	class USoundBase* HitSound;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
